daef277e16a375b4ee832fbf23b7c189b55f6177
Sullivown <daniel.w.t.sullivan@gmail.com>
2022-07-05 13:01:47 +0100

    add test data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import React, { useState } from 'react';
 import StartPage from './pages/StartPage';
 
 function App() {
+	const [score, setScore] = useState(0);
+	const [questions, setQuestions] = useState([]);
+	const [currentPage, setCurrentPage] = useState('start');
+	const [currentQuestion, setCurrentQuestion] = useState(1);
+
 	return <StartPage />;
 }
 
