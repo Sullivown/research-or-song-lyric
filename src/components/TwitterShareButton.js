33377fb32ec88d47a57ae68f969aac00127f94c2
Sullivown <daniel.w.t.sullivan@gmail.com>
2022-07-13 14:35:22 +0100

    removed prop-types import error

--- src/components/TwitterShareButton.js
+++ src/components/TwitterShareButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import PropTypes, { nominalTypeHack } from 'prop-types';
+import PropTypes from 'prop-types';
 
 import Button from '../elements/Button';
 
